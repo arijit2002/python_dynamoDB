b0563d5b3c82bf63abf31b9f9da483fe7037b054
Arijit Das <arijit10das2002@gmail.com>
2025-08-27 14:35:39 +0530

    Refactor item handling in Flask app and improve DynamoDB integration

--- index.py
+++ index.py
@@ -1,39 +1,42 @@
 import boto3
 import os
 from dotenv import load_dotenv
-load_dotenv()
-from flask import Flask
+from flask import Flask, request, jsonify
 
+load_dotenv()
 app = Flask(__name__)
 
+dynamodb_table = None
+
 def init_db():
+    global dynamodb_table
     dynamodb = boto3.resource(
-    'dynamodb',
-    aws_access_key_id=os.getenv("aws_access_key_id"),
-    aws_secret_access_key=os.getenv("aws_secret_access_key"),
-    region_name="eu-north-1"
+        'dynamodb',
+        aws_access_key_id=os.getenv("aws_access_key_id"),
+        aws_secret_access_key=os.getenv("aws_secret_access_key"),
+        region_name="eu-north-1"
     )
+    dynamodb_table = dynamodb.Table('projectH_db')
+    print("Table Status: ", dynamodb_table.table_status)
 
-    table= dynamodb.Table('projectH_db')
-    print("Table Status: ", table.table_status)
-
-    table.put_item(
-        Item={
-            'id': '456',
-            'name': 'Arijit',
-            'msg': 'Testing2'
-        }
-    )
-
-    response = table.get_item(
-     Key={'id': '456'}
-    )
-
-    print(response.get('Item'))
-    
 @app.route('/')
 def home():
     return "Welcome to the DynamoDB Flask App!"
+
+@app.route('/item', methods=['POST'])
+def add_item():
+    data = request.json
+    dynamodb_table.put_item(Item=data)
+    return jsonify({"message": "Item added", "item": data}), 201
+
+@app.route('/item/<id>', methods=['GET'])
+def get_item(id):
+    response = dynamodb_table.get_item(Key={'id': id})
+    item = response.get('Item')
+    if item:
+        return jsonify(item)
+    else:
+        return jsonify({"error": "Item not found"}), 404
 
 if __name__ == '__main__':
     init_db()
