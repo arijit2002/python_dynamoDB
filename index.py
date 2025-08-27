446433904e07f0f255efa11c1fe0d4466fd88d7f
Arijit Das <arijit10das2002@gmail.com>
2025-08-27 13:42:22 +0530

    Refactor Flask app structure and initialize DynamoDB connection

--- index.py
+++ index.py
@@ -2,28 +2,39 @@ import boto3
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from flask import Flask
 
-dynamodb = boto3.resource(
+app = Flask(__name__)
+
+def init_db():
+    dynamodb = boto3.resource(
     'dynamodb',
     aws_access_key_id=os.getenv("aws_access_key_id"),
     aws_secret_access_key=os.getenv("aws_secret_access_key"),
     region_name="eu-north-1"
-)
+    )
 
-table= dynamodb.Table('projectH_db')
+    table= dynamodb.Table('projectH_db')
+    print("Table Status: ", table.table_status)
 
-print("Table Status: ", table.table_status)
+    table.put_item(
+        Item={
+            'id': '456',
+            'name': 'Arijit',
+            'msg': 'Testing2'
+        }
+    )
 
-table.put_item(
-    Item={
-        'id': '456',
-        'name': 'Arijit',
-        'msg': 'Testing2'
-    }
-)
+    response = table.get_item(
+     Key={'id': '456'}
+    )
 
-response = table.get_item(
-    Key={'id': '456'}
-)
+    print(response.get('Item'))
+    
+@app.route('/')
+def home():
+    return "Welcome to the DynamoDB Flask App!"
 
-print(response.get('Item'))
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
